2a48dfc494cf4098413961b00bbd243d977a90db
Eddiebee <edbeeotudor@gmail.com>
2022-10-26 17:57:18 +0100

    Initial app setup

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { ApolloServer, gql } from "apollo-server";
+import { typeDefs } from "./schema";
+import { Query } from "./resolvers";
+
+const server = new ApolloServer({ typeDefs, resolvers: { Query } });
+
+server.listen().then(({ url }) => {
+  console.log(`
+        🚀 Server running on ${url}
+    `);
+});
