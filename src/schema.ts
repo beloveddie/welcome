d0eba19472acf7940760e1aa8618a0be1cf11b31
Eddiebee <edbeeotudor@gmail.com>
2022-10-31 14:40:54 +0100

    Add type definitions and fields

--- src/schema.ts
+++ src/schema.ts
@@ -4,4 +4,27 @@ export const typeDefs = gql`
   type Query {
     hello: String!
   }
+
+  type Mutation {
+    signin(signinInput: UserInput!): UserResponse!
+    signup(signupInput: UserInput!): UserResponse!
+  }
+
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+  }
+
+  input UserInput {
+    name: String
+    email: String!
+    password: String!
+  }
+
+  type UserResponse {
+    success: Boolean!
+    message: String!
+    user: User
+  }
 `;
